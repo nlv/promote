11c30479426f2482395df031400059493594d308
Leon Nikitin <nlv@lab321.ru>
2019-03-14 13:51:01 +0600

    меню первой страницы

--- clay/src/Stylesheets.hs
+++ clay/src/Stylesheets.hs
@@ -5,48 +5,114 @@ module Stylesheets
     ) where
 
 import Clay
+import Prelude hiding (rem)
 
--- fontName = "Raleway"
-fontName = "Amatic SC"
+rootFontSize = px 12
+rootFontName = "Raleway"
+-- fontName = "Amatic SC"
 
-secondaryFontName = serif
+secondaryFontName = sansSerif
+
+navbarHeight = px 95
+navbarHPadding = pct 10
+navbarColor = rgb 251 166 40
 
 -- FIXME: дожна коррелировать с высотой навигационной панели 
-navbarHeight = (px 105)
+-- navbarHeight = (px 105)
 
 mainStylesheet :: Bool -> Css
 mainStylesheet debug = do
-    importUrl "../fonts/Raleway-Regular.ttf"
-    importUrl "../fonts/Raleway-Regular-Italic.ttf"
-    importUrl "../fonts/Raleway-Bold.ttf"
-    importUrl "../fonts/Raleway-Bold-Italic.ttf"
-
-    importUrl "https://fonts.googleapis.com/css?family=Amatic+SC:400,700&subset=cyrillic"
+    importUrl "../fonts/raleway/raleway.css"
 
     -- Для корректной прокрутки разделов (из доки bootstrap scroll-spy) 
     body ? do
-        position relative 
-        fontFamily [fontName] [secondaryFontName]
+        fontSize rootFontSize 
+        fontFamily [rootFontName] [secondaryFontName]
+
+    "#start-page" ? do
+        backgroundSize cover
+        backgroundImage $ url "../img/start_page_cover.jpg"
+
+    "#start-page" # before ? do
+        content $ stringContent ""
+        position absolute
+        top nil
+        left nil
+        right nil
+        bottom nil
+        backgroundColor $ rgba 0 0 0 0.6
 
     "#navbar" ? do 
         if debug then border solid (px 1) blue else border inherit inherit inherit
+
+        display flex
+        alignItems center
+        justifyContent spaceBetween
+
+        minHeight navbarHeight
+        position fixed
+        top nil
+        right nil
+        left nil
+
+        paddingLeft navbarHPadding
+        paddingRight navbarHPadding
+
+        color navbarColor
+        fontWeight $ weight 600
+        backgroundColor $  rgba 0 0 0 0.05
+
+        fontSize $ em 1.1
+        textTransform uppercase
+
+        ul ? do
+            display flex
+            alignItems center
+            justifyContent spaceBetween
+
+            width (pct 40)
+
+            li ? do
+                borderColor navbarColor
+                borderBottomWidth $ px 1
+                -- sym borderRadius $ px 15
+                -- borderStyle solid
+
+                paddingBottom $ px 8
+                paddingTop $ px 8
+
+            li # ".active" ? do
+                borderColor navbarColor
+                borderBottomWidth $ px 1
+                borderStyle solid
+                color red
+
+
+            a ? do
+                textDecoration none
+                color navbarColor
+
+    "#navbar-phone" ? do
+        fontSize $ rem 1.2
+
 
     -- "#navbar" ? do 
     --     /* FIXME: или через container-fluid */
     --     /* margin-right: 0; */
     -- }
 
-    "#start-page" ? do
-        backgroundSize cover
-        backgroundImage $ url "../img/start_page_cover.jpg"
+    -- "#start-page" ? do
 
-    -- /* FIXME: убирать на малых экранах как navbar-toggler */
-    -- #start-phone {
-    --     /* margin-right: 50px; */
-    -- }
+    -- -- /* FIXME: убирать на малых экранах как navbar-toggler */
+    -- -- #start-phone {
+    -- --     /* margin-right: 50px; */
+    -- -- }
+
+    ".navbar-toggler" ? do
+        display none
 
     ".page" ? do
         height $ vh 100
-        width $ vw 100
+        -- width $ vw 100
 
-        paddingTop navbarHeight
+        paddingTop (navbarHeight @+@ (px 5))
--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
 $(document).ready(function(){
-    $("#collapsibleNavbar a, .navbar-brand").on('click', function(event){
 
+    $('#navbar li').click (function () {
+        $('#navbar li').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('#logo').click (function () {
+        $('#navbar li').removeClass('active');
+        $('#navbar li:first').addClass('active');
+    });
+
+
+
+    $("#navbar a, #logo").on('click', function(event){
+
+        let offset = $('body').data('offset')
+        console.log(offset);
         // Prevent default anchor click behavior
         event.preventDefault();
 
         // Store hash (#)
         var hash = this.hash;
+        console.log('hash: ');
+        console.log(hash);
 
         // Using jQuery's animate() method to add smooth page scroll
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
         $('html, body').animate({
             scrollTop: $(hash).offset().top
-        }, 800, function(){
+        }, 1200, function(){
+
 
             // Add hash (#) to URL when done scrolling (default click behavior)
             window.location.hash = hash;
