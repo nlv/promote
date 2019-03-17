3a7d9d2e7b70efdae7a100266bcead7ae17ae145
Leon Nikitin <nlv@lab321.ru>
2019-03-17 15:14:57 +0600

    Адаптивный navbar

--- clay/src/Stylesheets.hs
+++ clay/src/Stylesheets.hs
@@ -6,6 +6,7 @@ module Stylesheets
 
 import Clay
 import qualified Clay.Flexbox as Flexbox (flex)
+import qualified Clay.Media as Media
 import Prelude hiding (rem, (**))
 
 rootFontSize = px 12
@@ -14,16 +15,22 @@ rootFontName = "Raleway"
 
 secondaryFontName = sansSerif
 
+-- FIXME: дожна коррелировать с высотой навигационной панели 
 navbarHeight = px 95
 navbarHPadding = pct 10
 navbarColor = rgb 251 166 40
 
--- FIXME: дожна коррелировать с высотой навигационной панели 
--- navbarHeight = (px 105)
-
 mainStylesheet :: Bool -> Css
 mainStylesheet debug = do
-    let debugBorder = if debug then border solid (px 1) blue else border inherit inherit inherit
+    let debugIt borderC backgroundC = 
+            if debug 
+                then border solid (px 1) borderC >> backgroundColor backgroundC
+                else border inherit inherit inherit >> backgroundColor inherit
+        debugBorder = flip debugIt $ inherit
+        debugBack = debugIt inherit
+
+    -- let debugBack c = 
+    --     if debug then backgroundColor c else backgroundColor inherit
     importUrl "../fonts/raleway/raleway.css"
 
     -- Для корректной прокрутки разделов (из доки bootstrap scroll-spy) 
@@ -31,80 +38,85 @@ mainStylesheet debug = do
         fontSize rootFontSize 
         fontFamily [rootFontName] [secondaryFontName]
 
-    "#start-page" ? do
-        debugBorder
-
+    ".page" ? do
         height $ vh 100
+
+    "#p1" ? debugBack red
+    "#p2" ? debugBack green
+    "#p3" ? debugBack blue
+    "#p4" ? debugBack yellow
+
+    "#start-page" ? do
+        debugBorder blue 
 
         backgroundSize cover
         backgroundImage $ url "../img/start_page_cover.jpg"
         backgroundAttachment attachFixed
 
-        -- backgroundColor black
-        -- opacity 0.995
-
-        paddingTop nil
-
         display flex
         justifyContent center
         alignItems center
 
-        header ? do
-            width (pct 55)
-
-            -- marginTop $ pct (-20)
-
-            h1 ? do
-                debugBorder
-
-                textAlign center
-                fontSize $ em 3.1
-                paddingBottom $ px 10
-
-                color $ lightgoldenrodyellow
-                color $ lightgray
-
-            h2 ? do
-                debugBorder
-
-                display flex
-
-                paddingBottom $ px 10
-
-                color $ setA 0.7 navbarColor
+    --     paddingTop nil
 
 
+    --     header ? do
+    --         width (pct 55)
 
-            h2 ** ul ? do
-                debugBorder
+    --         -- marginTop $ pct (-20)
 
-                marginLeft auto
-                marginRight auto
+    --         h1 ? do
+    --             debugBorder
 
-                display flex
-                width $ pct 100
-                justifyContent center
+    --             textAlign center
+    --             fontSize $ em 3.1
+    --             paddingBottom $ px 10
 
-                fontSize $ em 1.8
+    --             color $ lightgoldenrodyellow
+    --             color $ lightgray
 
-                li ? do
-                    paddingLeft $ px 15
-                    paddingRight $ px 15
+    --         h2 ? do
+    --             debugBorder
 
-                    display inlineBlock
+    --             display flex
 
-                    -- color lightgray
+    --             paddingBottom $ px 10
+
+    --             color $ setA 0.7 navbarColor
+
+
+
+    --         h2 ** ul ? do
+    --             debugBorder
+
+    --             marginLeft auto
+    --             marginRight auto
+
+    --             display flex
+    --             width $ pct 100
+    --             justifyContent center
+
+    --             fontSize $ em 1.8
+
+    --             li ? do
+    --                 paddingLeft $ px 15
+    --                 paddingRight $ px 15
+
+    --                 display inlineBlock
+
+    --                 -- color lightgray
 
 
 
     "#navbar" ? do 
-        debugBorder
+        debugBorder green
 
         display flex
         alignItems center
         justifyContent spaceBetween
 
-        minHeight navbarHeight
+
+        -- minHeight navbarHeight
         position fixed
         top nil
         right nil
@@ -121,123 +133,165 @@ mainStylesheet debug = do
         fontSize $ em 1.1
         textTransform uppercase
 
-        ul ? do
-            display flex
-            alignItems center
-            justifyContent spaceBetween
-
-            width (pct 40)
-
-            li ? do
-                borderColor navbarColor
-                borderBottomWidth $ px 1
-
-                paddingBottom $ px 8
-                paddingTop $ px 8
-
-            li # ".active" ? do
-                borderColor navbarColor
-                borderBottomWidth $ px 1
-                borderStyle solid
-                color red
-
-            a ? do
-                textDecoration none
-                color navbarColor
-
-    "#navbar-phone" ? do
-        fontSize $ rem 1.2
-
-
-    ".navbar-toggler" ? do
-        display none
-
-
-    ".page" ? do
+    ".navbar-nav" ? do
         display flex
-        flexDirection column
-
-        width $ pct 70
-        height $ vh 100
-        marginLeft auto
-        marginRight auto
-        paddingTop (navbarHeight @+@ (px 5))
-
-        -- justifyContent center
-
-        hgroup ? do
-            -- debugBorder
-
-            paddingTop $ px 20
-            paddingBottom $ px 20
-
-
-        h1 ? do
-            -- debugBorder
-
-            sym padding $ px 15
-
-            fontSize $ rem 2.2
-            -- fontWeight $ weight 600
-            textTransform uppercase
-
-
-            textAlign center
-
-        h2 ? do
-            -- debugBorder
-
-            sym padding $ px 15
-
-            fontSize $ rem 1.6
-            textTransform uppercase
-
-            color $ rgba 0 0 0 0.5
-
-            textAlign center
-
-    ".cards" ? do
-        display flex
+        alignItems center
         justifyContent spaceBetween
 
-        paddingTop $ px 5
-        paddingBottom $ px 15
+        width (pct 40)
 
-    ".card" ? do 
-        width $ px 280
-        height $ px 400
+        li ? do
+            borderColor navbarColor
+            borderBottomWidth $ px 1
 
-        display flex
-        flexDirection column
-        alignItems center
+            paddingBottom $ px 8
+            paddingTop $ px 8
 
-        borderColor navbarColor
-        borderWidth $ px 4
-        borderStyle solid
+        li # ".active" ? do
+            borderColor navbarColor
+            borderBottomWidth $ px 1
+            borderStyle solid
+            color red
 
-        sym padding $ px 10
+        a ? do
+            textDecoration none
+            color navbarColor
 
-        hgroup ? do
 
-            -- FIXME пересекается с правилом выше надо делать a > b
-            paddingTop $ px 0
-            paddingBottom $ px 0
+    ".navbar-phone" ? do
+        fontSize $ rem 1.2
 
-            figure ? do
-                textAlign center
-                img ? do
-                    width $ pct 70
+    ".navbar-toggle" ? do
+        display none
+        borderStyle none
+        color navbarColor
+        backgroundColor transparent
+        fontSize $ rem 1.8
 
-            h1 ? do
-                debugBorder
-                fontSize $ em 1.9
-                paddingTop $ px 5
-                textAlign center
+    query Media.screen [Media.maxWidth $ px 1280] $ do
+        "#navbar" ? do
+            paddingLeft $ px 10
+            paddingRight $ px 10
 
-        p ? do
-            debugBorder
-            fontSize $ em 1.6
-            paddingTop $ px 5
-            paddingBottom $ px 5
-            textAlign center
+        ".brand" ? do
+            transform $ scale 0.7 0.7
+
+    query Media.screen [Media.maxWidth $ px 835] $ do
+        ".navbar-nav" ? do
+            width (pct 80)
+
+        ".navbar-phone" ? do
+            display none
+
+    -- query Media.screen [Media.maxWidth $ px 768] $ do
+    --     ".navbar-nav" ? do
+    --         width (pct 80)
+
+    --     ".navbar-phone" ? do
+    --         display none
+
+    query Media.screen [Media.maxWidth $ px 480] $ do
+
+        "#navbar" ? do
+            -- paddingLeft $ px 5
+            -- paddingRight $ px 5
+
+            ".navbar-nav" ? do
+                display none
+
+            -- ".navbar-phone" ? do
+            --     display none
+
+            ".navbar-toggle" ? do
+                display inlineBlock
+
+
+
+    -- ".page" ? do
+    --     display flex
+    --     flexDirection column
+
+    --     width $ pct 70
+    --     height $ vh 100
+    --     marginLeft auto
+    --     marginRight auto
+    --     paddingTop (navbarHeight @+@ (px 5))
+
+    --     -- justifyContent center
+
+    --     hgroup ? do
+    --         -- debugBorder
+
+    --         paddingTop $ px 20
+    --         paddingBottom $ px 20
+
+
+    --     h1 ? do
+    --         -- debugBorder
+
+    --         sym padding $ px 15
+
+    --         fontSize $ rem 2.2
+    --         -- fontWeight $ weight 600
+    --         textTransform uppercase
+
+
+    --         textAlign center
+
+    --     h2 ? do
+    --         -- debugBorder
+
+    --         sym padding $ px 15
+
+    --         fontSize $ rem 1.6
+    --         textTransform uppercase
+
+    --         color $ rgba 0 0 0 0.5
+
+    --         textAlign center
+
+    -- ".cards" ? do
+    --     display flex
+    --     justifyContent spaceBetween
+
+    --     paddingTop $ px 5
+    --     paddingBottom $ px 15
+
+    -- ".card" ? do 
+    --     width $ px 280
+    --     height $ px 400
+
+    --     display flex
+    --     flexDirection column
+    --     alignItems center
+
+    --     borderColor navbarColor
+    --     borderWidth $ px 4
+    --     borderStyle solid
+
+    --     sym padding $ px 10
+
+    --     hgroup ? do
+
+    --         -- FIXME пересекается с правилом выше надо делать a > b
+    --         paddingTop $ px 0
+    --         paddingBottom $ px 0
+
+    --         figure ? do
+    --             textAlign center
+    --             img ? do
+    --                 width $ pct 70
+
+    --         h1 ? do
+    --             debugBorder
+    --             fontSize $ em 1.9
+    --             paddingTop $ px 5
+    --             textAlign center
+
+    --     p ? do
+    --         debugBorder
+    --         fontSize $ em 1.6
+    --         paddingTop $ px 5
+    --         paddingBottom $ px 5
+    --         textAlign center
 
