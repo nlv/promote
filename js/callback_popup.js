10b5c6735d9d5abafa0bd1bbb3694c6331ebbc3a
Leon Nikitin <nlv@lab321.ru>
2019-06-20 09:48:39 +0600

    отправка формы (localhost)

--- js/callback_popup.js
+++ js/callback_popup.js
@@ -9,6 +9,23 @@ $( function() {
             buttons: {
             "Отправить заявку": function() {
                 $( this ).dialog( "close" );
+                var payload = {
+                    callbackName: $('#callback-popup input[name=name]').val(),
+                    callbackPhone: $('#callback-popup input[name=phone]').val()
+                };
+                
+                fetch("https://localhost:8080/api/callback",
+                {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                      },
+                    method: "POST",
+                    // body: data
+                    body: JSON.stringify(payload)
+                })
+                .then(function(res){ console.log(res); return res.json(); })
+                .then(function(data){ alert( JSON.stringify( data ) ) })                
             },
             "Отмена": function() {
                 $( this ).dialog( "close" );
