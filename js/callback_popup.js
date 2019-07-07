44ac24ceab3fde52ba2e6fc2eaeed5dd5cf3b5c3
Leon Nikitin <nlv@lab321.ru>
2019-07-08 00:45:20 +0600

    отправка формы  из руки

--- js/callback_popup.js
+++ js/callback_popup.js
@@ -9,23 +9,10 @@ $( function() {
             buttons: {
             "Отправить заявку": function() {
                 $( this ).dialog( "close" );
-                var payload = {
-                    callbackName: $('#callback-popup input[name=name]').val(),
-                    callbackPhone: $('#callback-popup input[name=phone]').val()
-                };
-                
-                fetch("http://robin-bol.ru:4321/api/callback",
-                {
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                      },
-                    method: "POST",
-                    // body: data
-                    body: JSON.stringify(payload)
-                })
-                .then(function(res){ console.log(res); return res.json(); })
-                .then(function(data){ alert( JSON.stringify( data ) ) })                
+
+                var name  = $('#callback-popup input[name=name]').val();
+                var phone = $('#callback-popup input[name=phone]').val();
+                sendCallback(name, phone);
             },
             "Отмена": function() {
                 $( this ).dialog( "close" );
@@ -38,4 +25,31 @@ $( function() {
         event.preventDefault();
         callbackPopup.dialog('open')
     })
+
+    $('#page-callback2-phone-submit').change(function() {
+        var name  = $('#page-callback2-popup input[name=name]').val();
+        var phone = $('#page-callback2-popup input[name=phone]').val();
+        sendCallback(name, phone);
+
+    })
 });
+
+function sendCallback (name, phone) {
+    var payload = {
+        callbackName: name,
+        callbackPhone: phone
+    };
+    fetch("http://robin-bol.ru:4321/api/callback",
+    {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+        method: "POST",
+        // body: data
+        body: JSON.stringify(payload)
+    })
+    .then(function(res){ return res.json(); })
+    .then(function(data){ alert( 'Заявка отправлена!'); })                
+    .catch(function() {alert('Отправка заявок временно недоступна. Позвоните сами!')})
+}
